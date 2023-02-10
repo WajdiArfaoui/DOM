2c5094cc727dd3bb145c81bb5ba508c174f28dda
WajdiArfaoui <116961441+WajdiArfaoui@users.noreply.github.com>
2023-02-10 17:02:16 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var favs = Array.from(document.querySelectorAll(".fa-heart"))
+var plusbtns = Array.from(document.querySelectorAll(".fa-plus-circle"))
+var minusbtns = Array.from(document.querySelectorAll(".fa-minus-circle"))
+var trashs = array.from(document.querySelectorAll(".fa-trash-alt"))
+
+for (let fav of favs)(
+    fav.addEventListener("click", function(){
+        if (fav.style.color==black)
+    }
+)
+
+  
